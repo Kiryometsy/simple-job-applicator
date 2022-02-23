99081c4b0bc2adb0b194e10b6dbec5e4347a8f3d
Kiryometsy <kiryometsy@gmail.com>
2022-02-23 16:12:32 +0100

    Changed controller , .properties to yml file

--- src/main/java/com/kiryometsy/applicator/job/simple/controllers/BestOffersController.java
+++ src/main/java/com/kiryometsy/applicator/job/simple/controllers/BestOffersController.java
@@ -1,18 +1,31 @@
 package com.kiryometsy.applicator.job.simple.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
 
-@RestController("/best-offers")
+@RestController
+@RequestMapping("/best-offers")
 public class BestOffersController {
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @Value("${host.name}")
+    private  String hostName;
+
+
     @GetMapping
-    public String namedParams
-            (@RequestParam(name = "id") String dataId) {
+    public String namedParams(@RequestParam(value="page", required = false) String page)
+    {
+        // backend?criteria=seniority%3Dtrainee,junior%20requirement%3Djava&page=1
 
-        return "dataId: " + dataId;
+        webClientBuilder.build()
+                .get()
+                .uri(hostName + "backend?criteria=seniority%3Dtrainee,junior%20requirement%3Djava&page=1")
+                .retrieve();
+        return hostName + "backend?criteria=seniority%3Dtrainee,junior%20requirement%3Djava&page=1";
     }
-
-
 }
+
